0dee321a9557b1f6fc78c081137a23ff6b6053cc
Nishant Makkar <NMakkar@xavient.com>
2016-12-02 16:59:45 +0530

    modified:   src/test/java/com/xavient/framework/AppiumSuite/AppiumTest.java

--- src/test/java/com/xavient/framework/AppiumSuite/AppiumTest.java
+++ src/test/java/com/xavient/framework/AppiumSuite/AppiumTest.java
@@ -1,26 +1,14 @@
 package com.xavient.framework.AppiumSuite;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Hashtable;
-
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
 import com.xavient.framework.util.Constants;
-
 import com.xavient.framework.util.MobileHelper;
 import com.xavient.framework.util.TestCaseDataProvider;
 import com.xavient.framework.util.Utility;
 import com.xavient.framework.util.Xls_Reader;
-
 import org.apache.log4j.Logger;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
 
 public class AppiumTest {
 
